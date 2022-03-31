9268addfe9068b35047ad84fd6f720d466b2a149
ChloeTB <55082721+ChloeTB@users.noreply.github.com>
2022-03-31 09:09:26 +0200

    The Unspoken part of Communication

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
 
 // functions go here
 
+
 });
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+  document.body.style.overflow = "hidden";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.overflow = "auto";
+}
